82d285033b78722ab4f5a8f90700d0d1119c09d2
Bart van Laar <Bartvanlaar1997@gmail.com>
2016-06-01 09:56:30 +0200

    Toegevoegd menu bar

--- TopID/app/src/main/java/nl/vanlaar/bart/topid/Activity/HomescreenActivity.java
+++ TopID/app/src/main/java/nl/vanlaar/bart/topid/Activity/HomescreenActivity.java
@@ -3,6 +3,7 @@ package nl.vanlaar.bart.topid.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
@@ -16,12 +17,15 @@ public class HomescreenActivity extends AppCompatActivity {
     private TextView inbox;
     private TextView ideeën;
     private TextView instellingen;
+    private Toolbar toolbar;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         uitloggen = (TextView) findViewById(R.id.tv_homescreen_uitloggen);
         uitloggen.setOnClickListener(new View.OnClickListener() {
