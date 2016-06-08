31f8ca97a6ba19b2adb3efb6813a6111517336b9
heuker <heukertje@gmail.com>
2016-06-08 12:23:09 +0200

    MainActivity

--- TopID/app/src/main/java/nl/vanlaar/bart/topid/Activity/MainActivity.java
+++ TopID/app/src/main/java/nl/vanlaar/bart/topid/Activity/MainActivity.java
@@ -25,7 +25,6 @@ import nl.vanlaar.bart.topid.View.KlachtenAdapter;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<Idee> ideeënLijst;
-
     public static User LOGGED_IN_USER = new User("henk",R.drawable.gabenewell,0);
     public static final int IDEE_REQUESTCODE = 1337;
     public static boolean dataChanged = false;
@@ -51,24 +50,43 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ideeAdapter = new IdeeënAdapter(this, R.layout.fragment_main, IdeeënLijst.getInstance().sortByIdee());
-        klachtenAdapter = new KlachtenAdapter(this, R.layout.fragment_main, IdeeënLijst.getInstance().sortByKlacht());
-        ideeënLijst = IdeeënLijst.getInstance().getIdeeën();
+        //als er geen user ingelogt is ga dan terug naar de login pagina
+        if (ingelogd == false) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
 
         //set onze custom toolbar
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        setAdapter(-1);
 
-        //De checkboxes, er kan er maar één aangevinkt zijn
+        //koppelen aan views
         bestBox = (CheckBox) findViewById(R.id.cb_beste);
         nieuwBox = (CheckBox) findViewById(R.id.cb_nieuw);
+        menuButton = (ImageView) findViewById(R.id.iv_mainActivity_menu);
+        backArrow = (ImageView) findViewById(R.id.iv_iedeeën_toolbar_backbutton);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        lvIdeeën = (ListView) findViewById(R.id.lvIdeeën);
+        spinnerSort = (Spinner) findViewById(R.id.spinner_lijst_sorteer);
+
+        //initeer de adapters en de iedeeënlijst
+        ideeAdapter = new IdeeënAdapter(this, R.layout.fragment_main, IdeeënLijst.getInstance().sortByIdee());
+        klachtenAdapter = new KlachtenAdapter(this, R.layout.fragment_main, IdeeënLijst.getInstance().sortByKlacht());
+        ideeënLijst = IdeeënLijst.getInstance().getIdeeën();
+
+        //set de adapter van de spinner
+        final ArrayAdapter<CharSequence> adapterSort = ArrayAdapter.createFromResource(this, R.array.sort_lijst, android.R.layout.simple_spinner_item);
+        spinnerSort.setAdapter(adapterSort);
+
+        //start de activity met de iedeeën
+        lvIdeeën.setAdapter(ideeAdapter);
+
+        //Als een checkbox wordt aangevinkt zet de ander checkbox uit en laat de juist listview zien
         bestBox.setChecked(true);
         bestBox.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,8 +103,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //de menu knop
-        menuButton = (ImageView) findViewById(R.id.iv_mainActivity_menu);
+        //als er op de menu knop gedrukt wordt ga naar het menu
         menuButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -95,8 +112,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //de terug knop
-        backArrow = (ImageView) findViewById(R.id.iv_iedeeën_toolbar_backbutton);
+        //als er op de terug knop wordt gedrukt ga naar de vorige activity
         backArrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -104,18 +120,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-        fab = (FloatingActionButton) findViewById(R.id.fab);
-        lvIdeeën = (ListView) findViewById(R.id.lvIdeeën);
-        spinnerSort = (Spinner) findViewById(R.id.spinner_lijst_sorteer);
-
-        //als er geen user ingelogt is ga dan terug naar de login pagina
-        if (ingelogd == false) {
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
-        }
-
-
+        //als er op een idee wordt gedrukt start de showIdeeActivity met het idee
         lvIdeeën.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -124,26 +129,21 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        final ArrayAdapter<CharSequence> adapterSort = ArrayAdapter.createFromResource(this, R.array.sort_lijst, android.R.layout.simple_spinner_item);
-        spinnerSort.setAdapter(adapterSort);
 
+        //als een element in de spinner wordt aangedrukt zet dan de juist adapter aan de listview
         spinnerSort.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-                setAdapter(position);
-
-
+                if (position == 0) {
+                    lvIdeeën.setAdapter(ideeAdapter);
+                }
+                if (position == 1) {
+                    lvIdeeën.setAdapter(klachtenAdapter);
+                }
             }
-
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
-                /*
-                parent.setSelection(0);
-                ideeën = IdeeënLijst.sortByIdee();
-                ideeAdapter.notifyDataSetChanged();
-                */
+                //DO NOTHING
             }
         });
 
@@ -152,10 +152,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, MakeIdeeActivity.class);
-
                 // start de make idee activity
                 startActivity(intent);
-
                 // sluit de mainactivity af.
                 finish();
             }
@@ -164,25 +162,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     @Override
     protected void onResume() {
         super.onResume();
-
-
-
     }
-
-
-
-    private void setAdapter(int position_in_spinner){
-        if (position_in_spinner == 0) {
-
-            lvIdeeën.setAdapter(ideeAdapter);
-        }
-        if (position_in_spinner == 1) {
-            lvIdeeën.setAdapter(klachtenAdapter);
-        }
-    }
-
 }
